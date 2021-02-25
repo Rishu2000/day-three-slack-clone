8a2a8e85d65ee7827cfd71be8ca03416b38643f4
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-02-25 17:10:37 +0530

    Coloned the slack-colne repo.

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import './App.css';
+import {useEffect, useState} from "react"
+import Chat from "./components/Chat";
+import Login from "./components/Login";
+import styled from "styled-components";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import db from "./firebase";
+
+function App() {
+
+  const [rooms,setRooms] = useState([])
+
+  const getChannels = () => {
+    db.collection("rooms").onSnapshot((snapshot) => {
+      setRooms(snapshot.docs.map((doc) => {
+        return { id: doc.id, name:doc.data().name}
+      }))
+    })
+  }
+
+  useEffect(() => {
+    getChannels();
+  }, [])
+
+  return (
+    <div className="App">
+      <Router>
+        <Container>
+          <Header/>
+          <Main>
+            <Sidebar rooms={rooms}/>
+        <Switch>
+          <Route path="/room">
+            <Chat/>
+          </Route>
+          <Route path="/">
+            <Login/>
+          </Route>
+        </Switch>
+        </Main>
+        </Container>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+
+const Container = styled.div`
+  width:100%;
+  height:100vh;
+  display:grid;
+  grid-template-rows: 38px auto;
+`
+const Main = styled.div`
+  display:grid;
+  grid-template-columns: 260px auto;
+`
