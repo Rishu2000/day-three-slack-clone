e332909f62dfaba2011e4ae4a567c12b7b9fe8e2
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-02-25 19:47:49 +0530

    Added some icons to the ChatInput components.

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import styled from "styled-components"
 import SendIcon from '@material-ui/icons/Send';
+import CodeIcon from '@material-ui/icons/Code';
+import LinkIcon from '@material-ui/icons/Link';
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+import CropFreeIcon from '@material-ui/icons/CropFree';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import AttachmentIcon from '@material-ui/icons/Attachment';
 
 function ChatInput() {
     return (
@@ -8,11 +15,25 @@ function ChatInput() {
             <InputContainer>
                 <form>
                     <input type="text" placeholder="Message here..."/>
-                    <SendButton>
+                    {/* <SendButton>
                         <Send/>
-                    </SendButton>
+                    </SendButton> */}
                 </form>
             </InputContainer>
+            <ImageContainer>
+                <LeftImages>
+                    <ImgIcon><CodeIcon/></ImgIcon>
+                    <ImgIcon><LinkIcon/></ImgIcon>
+                    <ImgIcon><FormatListNumberedIcon/></ImgIcon>
+                    <ImgIcon><FormatListBulletedIcon/></ImgIcon>
+                    <ImgIcon><CropFreeIcon/></ImgIcon>
+                </LeftImages>
+                <RightImages>
+                    <ImgIcon><EmojiEmotionsIcon/></ImgIcon>
+                    <ImgIcon><AttachmentIcon/></ImgIcon>
+                    <SendIconImg/>
+                </RightImages>
+            </ImageContainer>
         </Container>
     )
 }
@@ -22,12 +43,13 @@ export default ChatInput
 const Container = styled.div`
     padding-left:20px;
     padding-right:20px;
-    padding-bottom:24px;
+    border:1px solid #8D8D8E;
+    border-radius:4px;
+    margin:20px;
 `
 
 const InputContainer =styled.div`
-    border:1px solid #8D8D8E;
-    border-radius:4px;
+border-bottom:1px solid #cccccc;
 
     form{
         display:flex;
@@ -71,3 +93,39 @@ const Send = styled(SendIcon)`
     color:#D9D9D9
 `
 
+const ImageContainer = styled.div`
+    display:flex;
+    justify-content:space-between;
+    padding-top:8px;
+    padding-bottom:4px;
+    color:#808080;
+`
+
+const LeftImages = styled.div`
+display:flex;
+`
+
+const RightImages = styled.div`
+display:flex;
+`
+
+const ImgIcon = styled.div`
+    padding-right:6px;
+    padding-left:6px;
+
+    :hover{
+        background:#cccccc;
+        border-radius:4px;
+        cursor:pointer;
+        color:black;
+    }
+`
+
+const SendIconImg = styled(SendIcon)`
+padding-right:6px;
+padding-left:6px;
+    :hover{
+        background:#148567;
+        color:white;
+    }
+`
